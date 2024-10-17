231a9968789f68245e0bb856b8864a40f47d7e9d
Caximorris <mjtamagni@hotmail.com>
2024-10-17 13:07:08 +0200

    improved styles and format 3

--- src/containers/Additional.tsx
+++ src/containers/Additional.tsx
@@ -4,7 +4,7 @@ const Additional: React.FC = () => {
             <h2 className="text-3xl tracking-tight text-gray-900 sm:text-3xl pb-4">Languages</h2>
             <dl>
                 <div className="border-t border-gray-200 pt-4">
-                    <dd className="mt-2 text-sm text-gray-700">
+                    <dd className="mt-2 text-sm text-gray-900">
                         <ul>
                             <li>Spanish - Native</li>
                             <li>Catalan - Native</li>
@@ -17,9 +17,9 @@ const Additional: React.FC = () => {
             <h2 className="text-3xl tracking-tight text-gray-900 sm:text-3xl pb-4 pt-6">Additional</h2>
             <dl>
                 <div className="border-t border-gray-200 pt-4">
-                    <dd className="mt-2 text-sm text-gray-700">
+                    <dd className="mt-2 text-sm text-gray-900">
                         <ul>
-                            <li> Driving licence: B</li>
+                            <li>Driving licence: B</li>
                             <li>Own personal car</li>
                         </ul>
                     </dd>
