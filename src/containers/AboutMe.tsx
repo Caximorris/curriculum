a550f92b12e3594445475444d39eeef5612a95e8
Caximorris <mjtamagni@hotmail.com>
2024-11-07 12:38:11 +0100

    about me fix

--- src/containers/AboutMe.tsx
+++ src/containers/AboutMe.tsx
@@ -5,15 +5,8 @@ const AboutMe: React.FC = () => {
     <section className="h-full w-full border-2 rounded-[0.75em] border-black dark:border-white px-10  py-6 shadow-dark-shadow dark:shadow-light-shadow bg-light-teal dark:bg-dark-teal">
       <div>
         <h2 className="text-3xl tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl pt-2">Full Stack Developer</h2>
-        <p className="mt-4 text-gray-900 dark:text-gray-100">I am a passionate about science and technology, I have
-          always been curious to understand how things work and
-          how they can be improved.
-          I have excellent problem solving skills and am able to find
-          effective and creative solutions in both individual and team
-          situations. Furthermore, I have proven leadership skills and
-          can lead and motivate teams towards the achievement of
-          common goals. I consider myself a very adaptable person
-          and able to quickly learn new concepts and technologies.
+        <p className="mt-4 text-gray-900 dark:text-gray-100">
+          As a Computer Engineer with a passion for web development, I specialize in TypeScript, React, and NestJS to build user-centered, efficient applications. Known for my problem-solving skills, I thrive in both independent and collaborative settings, delivering creative, scalable solutions. I’m adaptable, quick to learn, and committed to continuous improvement—always looking for new ways to add value to my team and projects.
         </p>
       </div>
     </section>
